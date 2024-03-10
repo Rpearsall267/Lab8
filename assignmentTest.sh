d8d565392433729f2490f147d58a75f0f014d653
rpearsall267 <rpearsall267@gmail.com>
2024-03-10 09:59:25 -0700

    updated bash script

--- assignmentTest.sh
+++ assignmentTest.sh
@@ -2,19 +2,76 @@
 
 
 score=0
-results=`grep -i -E "DogCreator" Dog.java`
+results=`grep -i -E "class dogcreator" Dog.java`
 if [ -n "$results" ] 
 then 
 ((score++))
 fi
 echo "$score"
-results=`grep -i -E "dog" Dog.java`
+
+results=`grep -i -E "class dog" Dog.java`
 if [ -n "$results" ]
 then
 ((score++))
 fi
+
+results=`grep -i -E "dog()" Dog.java`
+if [ -n "$results" ]
+then 
+((score++))
+fi 
+
+results=`grep -i -E "tostring()" Dog.java`
+if [ -n "$results" ]
+then 
+((score++))
+fi
+
+results=`grep -i -E "bark()" Dog.java`
+if [ -n "$results" ]
+then 
+((score++))
+fi
+
+results=`grep -i -E "getname" Dog.java`
+if [ -n "$results" ]
+then
+((score++))
+fi
+
+results=`grep -i -E "getbreed" Dog.java`
+if [ -n "$results" ]
+then
+((score++))
+fi
+
+results=`grep -i -E "getage" Dog.java`
+if [ -n "$results" ]
+then
+((score++))
+fi
+
+results=`grep -i -E "getfurcolor" Dog.java`
+if [ -n "$results" ]
+then
+((score++))
+fi
+
+results=`grep -i -E "getservicedog" Dog.java`
+if [ -n "$results" ]
+then
+((score++))
+fi
+
+
+
+#javac Dog.java
+#java DogCreator > output.txt
+
+
+
 echo "$score"
-if [ $score == 2 ]
+if [ $score -gt 1 ]
 then
 echo "It passed"
 else 
